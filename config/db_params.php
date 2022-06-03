0b922a1e72cc6b96b85b0f8a070783f3cc8fd824
simSim2021 <90506175+simSim2021@users.noreply.github.com>
2022-06-03 19:14:15 +0200

    Add files via upload

---
+++ config/db_params.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Данные для подключения к БД
+ */
+
+return array(
+    'host' => 'localhost',
+    'user' => 'root',
+    'db_name' => 'shop_master',
+    //'pass' => 'password'
+    'pass' => ''
+);
