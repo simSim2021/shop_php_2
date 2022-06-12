2e3f4826a42369f5664f7050198a7c6cd602677c
simSim2021 <90506175+simSim2021@users.noreply.github.com>
2022-06-12 11:08:47 +0200

    Update Db.php

--- components/Db.php
+++ components/Db.php
@@ -15,4 +15,39 @@ class Db {
 
         return $db;
     }
+    static function query($sql, $params = [])
+    {
+        $stmt = self::connect()->prepare($sql);
+
+        if (!empty($params)) {
+            foreach ($params as $key => $val) $stmt->bindValue(':'.$key, $val);
+        }
+
+        $stmt->execute();
+        return $stmt;
+    }
+
+    static function row($sql, $params = [])
+    {
+        $stmt = self::query($sql, $params);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    static function rowAll($sql, $params = [])
+    {
+        $stmt = self::query($sql, $params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+
+
+
+
+	//Get row count
+    public function rowCount(){
+        return $this->stmt->rowCount();
+    }
+
+
 }
